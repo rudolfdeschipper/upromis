86fa73135aa1e10225c4634a49a3f9784ccb6c3c
Rudolf de Schipper <rudolf.deschipper@hotmail.com>
2021-02-05 14:37:29 +0100

    update ZC2 batch files to new folder structure

--- generation/runZeroCodeBB.bat
+++ generation/runZeroCodeBB.bat
@@ -1,3 +1,3 @@
-"D:\Documents\Visual Studio 2015\Projects\ZeroCode2\ZeroCode2\bin\Release\ZeroCode2"  --loglevel 2 --inputfile .\model\CRUDModel.zc --template .\template\MainFrontendTemplate.ct --outputpath ..\src --nowaitatexit
+"D:\Documents\Visual Studio 2015\Projects\ZeroCode2\ZeroCode2\bin\Release\ZeroCode2"  --loglevel 2 --inputfile .\model\CRUDModel.zc --template .\template\MainFrontendTemplate.ct --outputpath ..\front-end\src --nowaitatexit
 
-"D:\Documents\Visual Studio 2015\Projects\ZeroCode2\ZeroCode2\bin\Release\ZeroCode2"  --loglevel 2 --inputfile .\model\CRUDModel.zc --template .\template\MainBackendTemplate.ct --outputpath ..\..\..\upromisapi\uPromis.Microservice.ContractAPI
+"D:\Documents\Visual Studio 2015\Projects\ZeroCode2\ZeroCode2\bin\Release\ZeroCode2"  --loglevel 2 --inputfile .\model\CRUDModel.zc --template .\template\MainBackendTemplate.ct --outputpath ..\back-end\uPromis.Microservice.ContractAPI

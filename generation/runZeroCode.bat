6e952ed63783bb11544a9112e025e2a2b921dd25
Rudolf de Schipper <rudolf.deschipper@hotmail.com>
2021-02-05 18:39:49 +0100

    Add zerocode to the folder to have one consolidated set

--- generation/runZeroCode.bat
+++ generation/runZeroCode.bat
@@ -1,3 +1,3 @@
-"..\..\..\ZeroCode2\ZeroCode2\bin\Release\ZeroCode2.exe"  --loglevel 2 --inputfile .\model\CRUDModel.zc --template .\template\MainFrontendTemplate.ct --outputpath ..\front-end\src --nowaitatexit
+".\zerocode\ZeroCode2.exe"  --loglevel 2 --inputfile .\model\CRUDModel.zc --template .\template\MainFrontendTemplate.ct --outputpath ..\front-end\src --nowaitatexit
 
-"..\..\..\ZeroCode2\ZeroCode2\bin\Release\ZeroCode2.exe"  --loglevel 2 --inputfile .\model\CRUDModel.zc --template .\template\MainBackendTemplate.ct --outputpath ..\back-end\
+".\zerocode\ZeroCode2.exe"  --loglevel 2 --inputfile .\model\CRUDModel.zc --template .\template\MainBackendTemplate.ct --outputpath ..\back-end\
--- generation/runZeroCodeBB.bat
+++ generation/runZeroCodeBB.bat
@@ -1,3 +1,3 @@
-"D:\Documents\Visual Studio 2015\Projects\ZeroCode2\ZeroCode2\bin\Release\ZeroCode2"  --loglevel 2 --inputfile .\model\CRUDModel.zc --template .\template\MainFrontendTemplate.ct --outputpath ..\front-end\src --nowaitatexit
+".\zerocode\ZeroCode2"  --loglevel 2 --inputfile .\model\CRUDModel.zc --template .\template\MainFrontendTemplate.ct --outputpath ..\front-end\src --nowaitatexit
 
-"D:\Documents\Visual Studio 2015\Projects\ZeroCode2\ZeroCode2\bin\Release\ZeroCode2"  --loglevel 2 --inputfile .\model\CRUDModel.zc --template .\template\MainBackendTemplate.ct --outputpath ..\back-end\
+".\zerocode\ZeroCode2"  --loglevel 2 --inputfile .\model\CRUDModel.zc --template .\template\MainBackendTemplate.ct --outputpath ..\back-end\

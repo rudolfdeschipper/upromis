e5c66e4f1ea6ed49e371dbfba321d8ec4adec9cc
Rudolf de Schipper <rudolf.deschipper@hotmail.com>
2021-02-21 18:28:19 +0100

    add missing reference

--- back-end/upromis.Services/DTOBase.cs
+++ back-end/upromis.Services/DTOBase.cs
@@ -1,10 +1,4 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace uPromis.Microservice.DTO.Models
+﻿namespace uPromis.Microservice.DTO.Models
 {
     public class DTOBase
     {

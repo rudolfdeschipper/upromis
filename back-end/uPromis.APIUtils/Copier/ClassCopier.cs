9001301d0a44c0a808d0b238d517d77b6495f009
Rudolf de Schipper <rudolf.deschipper@hotmail.com>
2021-04-05 16:40:50 +0200

    Adding property copier class

---
+++ back-end/uPromis.APIUtils/Copier/ClassCopier.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uPromis.APIUtils.Copier
+{
+    public class ClassCopier<TParent, TChild> where TParent : class
+                                              where TChild : class
+    {
+        public static void CopyClassProperties(TParent parent, TChild child)
+        {
+            var childProperties = child.GetType().GetProperties();
+            foreach (var childProperty in childProperties)
+            {
+                var attributesForProperty = childProperty.GetCustomAttributes(typeof(ClassCopierAttribute), true)
+                    .Where(ca => (ca as ClassCopierAttribute)?.ParentClass is TParent);
+
+                var isOfTypeMatchParentAttribute = false;
+
+                ClassCopierAttribute currentAttribute = null;
+
+                foreach (var attribute in attributesForProperty)
+                {
+                    if (attribute.GetType() == typeof(ClassCopierAttribute) && (attribute as ClassCopierAttribute).ParentClass is TParent)
+                    {
+                        isOfTypeMatchParentAttribute = true;
+                        currentAttribute = (ClassCopierAttribute)attribute;
+                        break;
+                    }
+                }
+
+                if (isOfTypeMatchParentAttribute)
+                {
+                    var parentProperties = parent.GetType().GetProperties();
+                    object parentPropertyValue = null;
+                    foreach (var parentProperty in parentProperties)
+                    {
+                        if (parentProperty.Name == currentAttribute.ParentPropertyName)
+                        {
+                            if (parentProperty.PropertyType == childProperty.PropertyType)
+                            {
+                                parentPropertyValue = parentProperty.GetValue(parent);
+                            }
+                        }
+                    }
+                    if (parentPropertyValue != null)
+                    {
+                        childProperty.SetValue(child, parentPropertyValue);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ back-end/uPromis.APIUtils/Copier/ClassCopierAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uPromis.APIUtils.Copier
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ClassCopierAttribute : Attribute
+    {
+        public readonly string ParentPropertyName;
+        public readonly Type ParentClass;
+        public ClassCopierAttribute(Type parenClass,  string parentPropertyName)
+        {
+            ParentPropertyName = parentPropertyName;
+            ParentClass = parenClass;
+        }
+    }
+}
